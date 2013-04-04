c43d493cda7ac924b36f6b5ff04c93756a9b2293
Philip Schleihauf <uniphil@gmail.com>
2013-04-04 19:09:00 -0400

    working on up

--- v3/hello.py
+++ v3/hello.py
@@ -31,11 +31,12 @@ Calama Consulting
 =================
 
 _Never Stop Innovating_
+
 """
 
 
 content['who_we_are'] = """
-The solar photovoltaic industry thrives on innovation. The potential for PV in
+<!--The solar photovoltaic industry thrives on innovation. The potential for PV in
 the world is huge, but so are the challenges. In the race to integrate solar as
 a major player of our sustainable energy mix those companies who do not
 innovate will be left behind; as long as the industry relies on unsustainable
@@ -43,7 +44,7 @@ government subsidies, there can be no status-quo.
 
 Calama Consulting is a solar systems consulting firm committed to continual
 innovation, and specializes in developing customized solutions for its clients.
-
+-->
 """
 
 
